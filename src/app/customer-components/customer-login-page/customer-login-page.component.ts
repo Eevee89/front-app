db8b97e9b9e0ca221368248a3df9913c0d5cc448
Eevee89 <jorismartin3@gmail.com>
2024-11-19 17:36:05 +0100

    Modify patient login page

--- src/app/customer-components/customer-login-page/customer-login-page.component.ts
+++ src/app/customer-components/customer-login-page/customer-login-page.component.ts
@@ -1,12 +1,20 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { Patient } from '../../../models/patient';
 
 @Component({
   selector: 'app-customer-login-page',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgClass],
   templateUrl: './customer-login-page.component.html',
   styleUrl: './customer-login-page.component.css'
 })
 export class CustomerLoginPageComponent {
+  patient?: Patient;
 
+  step: number = 0;
+
+  submit(step: number) {
+    this.step = step;
+  }
 }
