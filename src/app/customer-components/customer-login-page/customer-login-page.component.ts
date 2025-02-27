0bc4344ee8fe79e57256f419909952545d74443f
Eevee89 <jorismartin3@gmail.com>
2025-02-27 21:29:13 +0100

    Handle patient creation

--- src/app/customer-components/customer-login-page/customer-login-page.component.ts
+++ src/app/customer-components/customer-login-page/customer-login-page.component.ts
@@ -9,11 +9,12 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { HttpClient, HttpErrorResponse, HttpStatusCode } from '@angular/common/http';
+import { MatRadioModule } from '@angular/material/radio';
 import { firstValueFrom } from 'rxjs'; 
 
 @Component({
@@ -24,6 +25,7 @@ import { firstValueFrom } from 'rxjs';
     FormsModule, 
     ReactiveFormsModule,
     MatCardModule,
+    MatRadioModule,
     MatButtonModule,
     MatTabsModule,
     MatFormFieldModule,
@@ -36,7 +38,9 @@ import { firstValueFrom } from 'rxjs';
 })
 export class CustomerLoginPageComponent {
   hide = signal(true);
-  errorMessage = signal('');
+  emailErrorMessage = signal('');
+  phoneErrorMessage = signal('');
+
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
@@ -46,33 +50,26 @@ export class CustomerLoginPageComponent {
 
   signin?: boolean = false;
 
-  patient?: Patient = {
-    id: 1, 
-    firstName: "", 
-    lastName: "", 
-    gender: true, 
-    email: "", 
-    phone: "", 
-    addressId: 1
-  };
-
-  address?: Address = {
-    id: 1,
-    street: "",
-    city: "",
-    zipCode: ""
-  };
-
   step: number = 0;
+
+  gender: string = "M";
 
   readonly email = new FormControl('', [Validators.required, Validators.email]);
   readonly password = new FormControl('', [Validators.required]);
+  readonly firstName = new FormControl('', [Validators.required]);
+  readonly lastName = new FormControl('', [Validators.required]);
+  readonly birthDate = new FormControl('', [Validators.required]);
+  readonly street = new FormControl('', [Validators.required]);
+  readonly zipCode = new FormControl('', [Validators.required]);
+  readonly city = new FormControl('', [Validators.required]);
+  readonly phone = new FormControl('', [Validators.required, Validators.maxLength(10)]);
+  readonly confPassword = new FormControl('', [Validators.required]);
 
   constructor(private router: Router, private _httpClient: HttpClient){}
 
   private _snackBar = inject(MatSnackBar);
 
-  submit(step: number) {
+  /*submit(step: number) {
     if (step == 0) {
       this.step = step;
     }
@@ -85,7 +82,7 @@ export class CustomerLoginPageComponent {
         this.step = step;
       }
     }
-  }
+  }*/
 
   connect() {
     // Call endpoint
@@ -113,13 +110,23 @@ export class CustomerLoginPageComponent {
     this.signin = true;
   }
 
-  updateErrorMessage() {
+  updateEmailErrorMessage() {
     if (this.email.hasError('required')) {
-      this.errorMessage.set('You must enter a value');
+      this.emailErrorMessage.set('Vous ne pouvez pas laisser ce champ vide');
     } else if (this.email.hasError('email')) {
-      this.errorMessage.set('Not a valid email');
+      this.emailErrorMessage.set('L\'email n\'est pas valide');
     } else {
-      this.errorMessage.set('');
+      this.emailErrorMessage.set('');
+    }
+  }
+
+  updatePhoneErrorMessage() {
+    if (this.phone.hasError('required')) {
+      this.phoneErrorMessage.set('Vous ne pouvez pas laisser ce champ vide');
+    } else if (this.phone.hasError('maxlength')) {
+      this.phoneErrorMessage.set('Le numéro est trop long (max 10 caractères)');
+    } else {
+      this.phoneErrorMessage.set('');
     }
   }
 
@@ -137,8 +144,86 @@ export class CustomerLoginPageComponent {
     } catch (error) {
       if (error instanceof HttpErrorResponse) {
         if (error.status === HttpStatusCode.Unauthorized) {
-          let snackBarRef = this._snackBar.open(
+          this._snackBar.open(
             'Les informations fournies n\'ont pas permis de vous identifier.', '',
+            { duration: 3000 }
+          );
+        }
+      }
+    }
+  }
+
+  async signupClick() {
+    try {
+      if (/*!this.firstName.value || !this.lastName.value || !this.email.value 
+        || !this.phone.value || !this.birthDate.value
+        ||*/ !this.street.value || !this.zipCode.value || !this.city.value
+        /*|| !this.password.value || !this.confPassword.value*/) {
+        this._snackBar.open(
+          'Il manque des données vous concernant', undefined,
+          { duration: 3000 }
+        );
+      }
+      else if (this.password.value != this.confPassword.value) {
+        this._snackBar.open(
+          'Les mots de passe ne correspondent pas', undefined,
+          { duration: 3000 }
+        );
+      }
+      else {
+        let user = {
+          email: this.email.value,
+          password: this.password.value,
+        };
+
+        let toAddaddress: {
+          street: string;
+          zipCode: string;
+          city: string;
+        };
+        
+        toAddaddress = {
+          street: this.street.value!,
+          zipCode: this.zipCode.value!,
+          city: this.city.value!,
+        };
+
+        localStorage.setItem('userData', JSON.stringify(user));
+        
+        let resp = JSON.stringify(await firstValueFrom(this._httpClient.post('/api/address/create', toAddaddress)));
+        resp = resp.substring(6);
+        const n = resp.length;
+        resp = resp.substring(0, n-1);
+
+        let address: Address = {
+          id: parseInt(resp),
+          street: toAddaddress.street,
+          city: toAddaddress.city,
+          zipCode: toAddaddress.zipCode
+        };
+
+        let patient: Patient = {
+          firstName: this.firstName.value!, 
+          lastName: this.lastName.value!, 
+          gender: this.gender == "M", 
+          email: this.email.value!, 
+          phone: this.phone.value!,
+          birthDate: new Date(this.birthDate.value ? this.birthDate.value : "01/01/1970"), 
+          address: address,
+          password: this.password.value!
+        };
+
+        localStorage.setItem('user', JSON.stringify(patient));
+        await firstValueFrom(this._httpClient.post('/api/patients/create', patient))
+  
+        //this.router.navigate(["customer/index"]);
+      }
+    } catch (error) {
+      console.log(error);
+      if (error instanceof HttpErrorResponse) {
+        if (error.status === HttpStatusCode.Unauthorized) {
+          this._snackBar.open(
+            'Les informations fournies n\'ont pas permis de vous identifier.', undefined,
             { duration: 3000 }
           );
         }
--- src/models/patient.ts
+++ src/models/patient.ts
@@ -1,11 +1,12 @@
+import { Address } from './address';
+
 export interface Patient {
-    id: number;
     firstName: string;
     lastName: string;
     gender: boolean;
     phone: string;
     email: string;
-    addressId: number;
+    address: Address;
     birthDate?: Date;
     appointmentIds?: number[];
     password?: string;
